be1803ea79655a86a3e106e56f9551b4f7dbe4e3
ShpkUshnk <zgfzgfzgaf@gmail.com>
2026-03-09 20:05:37 +0700

    4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -78,7 +78,34 @@ blogRouter.put("/:id", async (request, response, next) => {
 });
 
 blogRouter.delete("/:id", async (request, response, next) => {
-  await Blog.findByIdAndDelete(request.params.id);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+
+  const user = await User.findById(decodedToken.id);
+
+  if (!user) {
+    return response.status(400).json({ error: "userId missing or not valid" });
+  }
+
+  const blog = await Blog.findById(request.params.id);
+
+  if (!blog) {
+    return response
+      .status(400)
+      .json({ error: "blog missing or already deleted" });
+  }
+
+  if (user.id.toString() !== blog.user.toString()) {
+    return response.status(401).json({
+      error: "blog can be deleted only by creator",
+    });
+  }
+
+  await blog.deleteOne();
+
   response.status(204).end();
 });
 
